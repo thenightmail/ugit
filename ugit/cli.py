7743b5de82e0396f842c22a81e5c728b5260e2d7
Andrew Chitester <thenightmail@proton.me>
2025-10-24 09:54:30 -0400

    cat-file: Print hashed objects

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -20,12 +21,22 @@ def parse_args():
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument('file')
 
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+
     return parser.parse_args()
 
 def init(args):
     data.init()
     print (f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
 
+
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object (f.read ()))
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object (args.object))
